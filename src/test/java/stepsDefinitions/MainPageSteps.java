5023664442bed3eeff6bf5946bff17563430aa43
Patryk Prentki <patryk.prentki90@gmail.com>
2025-07-18 23:57:07 +0200

    It`s a class which contains steps on MainPage

---
+++ src/test/java/stepsDefinitions/MainPageSteps.java
@@ -0,0 +1,23 @@
+package stepsDefinitions;
+
+import common.AppStructureNames;
+import io.cucumber.java.en.Given;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pageobject.MainPage;
+
+import static org.testng.Assert.assertTrue;
+
+public class MainPageSteps {
+
+    private final MainPage mainPage = new MainPage();
+    private static AppStructureNames names = new AppStructureNames();
+    private static final Logger logger = LogManager.getLogger(names.getAPP_NAME());
+
+
+    @Given("User checks if logged on correct website")
+    public void open() {
+        logger.info("Checking website title");
+        assertTrue(mainPage.verifyAppTextName(), "Website name is different");
+    }
+}
