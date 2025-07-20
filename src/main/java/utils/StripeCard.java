c24bda300aa67b49d8caebe532f2bc803cfc048d
Patryk Prentki <patryk.prentki90@gmail.com>
2025-07-20 23:59:49 +0200

    Added cardname field

--- src/main/java/utils/StripeCard.java
+++ src/main/java/utils/StripeCard.java
@@ -1,13 +1,15 @@
 package utils;
 
 import lombok.Builder;
+import lombok.Data;
 
 @Builder
+@Data
 public class StripeCard {
 
 
-    private final String CARD_NUMBER;
+    private final String CARD_NAME;
+    private final int CARD_NUMBER;
     private final String EXP_DATE;
-    private final String CVV;
-
+    private final int CVV;
 }
