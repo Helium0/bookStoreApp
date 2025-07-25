2d3a11f60b0f89232be55923ee1c35d5f3c94237
Patryk Prentki <patryk.prentki90@gmail.com>
2025-07-26 00:33:01 +0200

    Added junit properties and attached allure plugin

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/resources/features"},
-        glue = {"stepsDefinitions"}
+        glue = {"stepsDefinitions"},
+        plugin = {"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
 )
 
 public class TestRunner {
