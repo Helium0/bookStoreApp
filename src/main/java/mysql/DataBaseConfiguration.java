dc72636b803461cd1caaaab5185b800553d222a3
Patryk Prentki <patryk.prentki90@gmail.com>
2025-07-28 23:49:20 +0200

    Added logger for debugging

--- src/main/java/mysql/DataBaseConfiguration.java
+++ src/main/java/mysql/DataBaseConfiguration.java
@@ -2,17 +2,23 @@ package mysql;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import common.AppStructureNames;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.sql.DataSource;
 
 public class DataBaseConfiguration {
 
+    private static final AppStructureNames names = new AppStructureNames();
+    private static final Logger logger = LogManager.getLogger(names.getDATA_BASE());
 
     public static DataSource getDataSource() {
+        logger.info("Connecting to DB");
         HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3306/stripecards");
+        hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3307/wordpress");
         hikariConfig.setUsername("root");
-        hikariConfig.setPassword("admin");
+        hikariConfig.setPassword("root");
         hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
 
         return new HikariDataSource(hikariConfig);
