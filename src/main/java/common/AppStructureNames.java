39d09b47e79dc12a0a47a2503a7a500cabbfb2ac
Patryk Prentki <patryk.prentki90@gmail.com>
2025-07-28 23:50:18 +0200

    Added another class fields

--- src/main/java/common/AppStructureNames.java
+++ src/main/java/common/AppStructureNames.java
@@ -9,5 +9,11 @@ public class AppStructureNames {
     private final String APP_NAME = "[TEST_APP] ";
     private final String SHOP_NAME = APP_NAME + "[SHOP]";
     private final String CART_NAME = APP_NAME + "[CART]";
+    private final String DATA_BASE = APP_NAME + "[DATA-BASE]";
+    private final String USER_CARD = "[CARD]";
+    private final String USER = "[USER]";
+    private final String USER_BILLING_DETAILS = USER + " [BILLING-DETAILS]";
+    private final String ORDER = APP_NAME + "[ORDER-DETAILS]";
+    private final String FINAL_PAGE = APP_NAME + "[ORDER-RECEIVED]";
 
 }
--- src/main/java/common/CommonStructure.java
+++ src/main/java/common/CommonStructure.java
@@ -8,4 +8,5 @@ public class CommonStructure {
     private final String ADD_TO_CART_BUTTON = "[ADD TO CART]";
     private final String SEARCH_BUTTON = "[SEARCH]";
     private final String START_SHOPPING_BUTTON = "[START_SHOPPING]";
+    private final String BUTTON = "[BUTTON]";
 }
