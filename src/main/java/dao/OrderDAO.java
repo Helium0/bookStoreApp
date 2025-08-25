64f76e91c4b51e48415aef2ea21aeb3dec74ae67
Patryk Prentki <patryk.prentki90@gmail.com>
2025-08-25 22:10:58 +0200

    Added delete by ID

--- src/main/java/dao/OrderDAO.java
+++ src/main/java/dao/OrderDAO.java
@@ -15,6 +15,17 @@ public class OrderDAO implements StripeOrderDAO {
     private final DataSource dataSource;
 
     @Override
+    public Order deleteOrderById(String userOrderId) throws SQLException {
+        String getOrder = "DELETE FROM wordpress.wp_woocommerce_order_items " +
+                "WHERE order_id = " + userOrderId;
+        try (Connection connection = dataSource.getConnection();
+            Statement statement = connection.createStatement()) {
+            statement.executeUpdate(getOrder);
+        }
+        return null;
+    }
+
+    @Override
     public Order getOrderById(String userOrderId) throws SQLException {
         String getOrder = "SELECT * FROM wp_woocommerce_order_items WHERE order_id = " + userOrderId;
         try (Connection connection = dataSource.getConnection();
